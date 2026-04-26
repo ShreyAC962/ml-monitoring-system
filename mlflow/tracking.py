3fc6c404ee1ca23869d915cf4c01f18ad8baf543
Shreya C <97959780+ShreyAC962@users.noreply.github.com>
2026-04-26 15:40:24 -0700

    Tracks and returns highest accuracy of best run

---
+++ mlflow/tracking.py
@@ -0,0 +1,16 @@
+import mlflow
+
+def get_best_accuracy():
+    # Create an MLflow client to interact with experiment runs
+    client = mlflow.tracing.MlflowClient()
+
+    experiment = mlflow.get_experiment_by_name("ml-monitoring")
+
+
+    runs = client.search_runs(
+        experiment_ids=[experiment.experiment_id], # Look only inside this experiment
+        order_by=["metrics.accuracy DESC"] # Sort runs by an accuracy in descending order(best first)
+    )
+
+    # Return accuracy of the best run (top result)
+    return runs[0].data.metrics["accuracy"]
