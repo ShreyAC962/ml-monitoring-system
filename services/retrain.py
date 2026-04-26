87b37526b121163c6f4d16d2783ebd2cac7952fc
Shreya C <97959780+ShreyAC962@users.noreply.github.com>
2026-04-26 13:42:54 -0700

    Automatically retrains the model - comments added

--- services/retrain.py
+++ services/retrain.py
@@ -1,4 +1,6 @@
 import subprocess
+# Import subprocess module to run external commands/scripts
 
 def retrain():
+    # This executes: python model/train.py
     subprocess.run(["python", "model/train.py"])
